f4fc73dcf833762711d8f866a1f1c8ca7020729c
Tanmoy Sarker <tanmoysarker749@outlook.com>
2019-09-14 12:34:14 +0600

    minor

--- Index.js
+++ Index.js
@@ -1,18 +1,31 @@
-function imageshadow(options){
-    let images = document.querySelectorAll('.imageshadow');
+const  imageshadow = (options) => {
 
-    if(options.shadow_type === 'hard'){
-        options.shadow_type = '0px'
-    }
-    else options.shadow_type = '15px'
+    let images = document.querySelectorAll(".imageshadow");
 
-    images.forEach(image => {
-        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0,12)`
+        if(options.shadow_type === "hard")
 
-        if(options.padding){
-            image.style.padding = '1em';
+           options.shadow_type = "0px"
+
+        else
+
+           options.shadow_type = "15px" 
+
+           images.forEach(image => {
+
+               image.style.boxShadow = "10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12";
+
+
+
+               if(options.padding){
+
+                   image.style.padding = "1em"
+
+               }
+
+           })
+
         }
-    })
-}
 
-module.exports.imageshadow = imageshadow;
+
+
+        module.exports.imageshadow = imageshadow;
